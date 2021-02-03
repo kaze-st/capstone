badbed4a2112de2a5350a427b383b210a983cb43
haoc46 <haoc.46@gmail.com>
2021-02-02 21:35:24 -0800

    Tabs test

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 app.listen(3000, function () {
-	console.log('App is listening on port 3000!');
+		console.log('App is listening on port 3000!');
 });
