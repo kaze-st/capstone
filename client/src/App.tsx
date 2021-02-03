0c8691aca26ded06f3acbf059f7532ea3c84759f
haoc46 <haoc.46@gmail.com>
2021-02-03 01:04:59 -0800

    Added Client Starter Code

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      This is 
+    </div>
+  );
+}
+
+export default App;
