a8a1ce020a43e058fcdef54bff43e91836b1c748
Khoa Nguyen Luong <49085332+Kevov@users.noreply.github.com>
2021-02-11 00:17:27 +0700

    Revert "unify css"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,14 @@
-
-import './App.scss';
-import './components/login/Login.tsx';
 import './index.css';
 
 import LandingPage from './LandingPage/LandingPage'
 import React from 'react';
-import Routes from './routes';
-import { BrowserRouter  } from 'react-router-dom'; 
-
-import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-          <div className="App">
-            <Routes/>
-          </div>
-      </BrowserRouter>
-    </AuthProvider>
+    <div className="page-wrapper">
+      <LandingPage />
+    </div>
+  );
+}
 
 export default App;
