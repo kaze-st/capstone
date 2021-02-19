e68e2df90f05eb701cfe9c168835f4688f2ad3d6
haoc46 <haoc.46@gmail.com>
2021-02-18 19:26:58 -0800

    Polished code

--- client/src/firebase-config.ts
+++ client/src/firebase-config.ts
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import firebase from 'firebase/app';
 
 dotenv.config();
-console.log('env', process.env.REACT_APP_FIREBASE_API_KEY);
 const app = firebase.initializeApp({
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
--- client/src/types/UserContext.ts
+++ client/src/types/UserContext.ts
@@ -2,5 +2,5 @@ import firebase from 'firebase/app';
 
 export default interface UserContext {
 	firebaseUser: firebase.User | null | undefined;
-	idToken: any;
+	idToken: string;
 }
