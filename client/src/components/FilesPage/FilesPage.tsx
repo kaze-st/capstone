577c1330c21a7215d0d67ec1714f671989ce9b57
Khoa Luong <khoa.luong@yahoo.com.vn>
2021-03-01 13:40:10 +0700

    Draft

---
+++ client/src/components/FilesPage/FilesPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FilesPage(): JSX.Element {
+	return <></>
+}
+
+function SideNav(): JSX.Element {
+	return <nav>
+		
+	</nav>
+}
+
+function FileList(): JSX.Element {
+	return <>
+	
+	</>
+}
