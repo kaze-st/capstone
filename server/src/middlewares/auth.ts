1128d6b84c2ef60795ad59851718feaa179d154e
haoc46 <haoc.46@gmail.com>
2021-03-08 18:02:30 -0800

    Substituted firebase admin sdk to jwt to verify token

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,10 +1,9 @@
-import * as admin from 'firebase-admin';
-
+import axios from 'axios';
 import dotenv from 'dotenv';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
-const app = admin.initializeApp();
 
 export default async (
 	req: express.Request,
@@ -18,14 +17,29 @@ export default async (
 
 	const idToken = req.headers.authorization;
 	const errMessage = 'You do not have permissions to access this API';
-
 	if (!idToken) {
 		res.status(401).jsonp({ message: errMessage });
 		return;
 	}
 
 	try {
-		await app.auth().verifyIdToken(idToken);
+		const response = await axios.get(
+			'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com'
+		);
+
+		const publicKeys = response.data;
+		const header64 = idToken.split('.')[0];
+		const header = JSON.parse(
+			Buffer.from(header64, 'base64').toString('ascii')
+		);
+
+		//eslint-disable-next-line -- Verified Object can anything
+		const verificationObj: any = jwt.verify(idToken, publicKeys[header.kid], {
+			algorithms: ['RS256']
+		});
+
+		// TODO add UserContext
+		res.locals.uid = verificationObj.user_id;
 		next();
 	} catch (err) {
 		res.status(401).jsonp({
