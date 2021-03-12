a794927f1f36b51fab07be99389865ae5163cb88
Khoa Luong <khoa.luong@yahoo.com.vn>
2021-03-12 14:50:48 +0700

    stuff

--- client/src/components/FilesPage/Modal.tsx
+++ client/src/components/FilesPage/Modal.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import './Modal.scss';
 
-// interface IModalProp {
-// 	show: boolean;
-// }
-
-// const Modal: React.FC<IModalProp> = ({ show, children }): JSX.Element => {
-// 	const showHideClassName = show ? 'modal' : 'modal hide';
-// 	return (
-// 		<div className={showHideClassName}>
-// 			<div className="modal-container">{children}</div>
-// 		</div>
-// 	);
-// };
-
 interface IModalProp {
 	show: boolean;
 	children: JSX.Element | JSX.Element[] | string;
